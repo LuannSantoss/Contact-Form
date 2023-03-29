df6929a4f6862ee6184754274ed164e9be159eaa
Luan Santos <88041659+LuannSantoss@users.noreply.github.com>
2023-03-29 11:50:12 -0300

    Update contact_form.py

--- Contact_form/contact_form.py
+++ Contact_form/contact_form.py
@@ -19,4 +19,4 @@ def local_css(file_name):
      with open(file_name) as f:
           st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("Códigos\Projetos\Contact_form\style\style.css")
+local_css("style/style.css")
